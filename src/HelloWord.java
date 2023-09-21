5bb68ba7c8d1f6669d27e0c665857caa49097ab3
pietro.ravagni <pietro.ravagni@buonarroti.tn.it>
2023-09-21 14:10:53 +0200

    first: classe che gestisce il saluto.

---
+++ src/HelloWord.java
@@ -0,0 +1,10 @@
+public class HelloWord {
+    private String saluto;
+    public void helloWord (){
+        saluto="Hello world";
+    }
+
+    public String toString(){
+        return saluto;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HelloWord ob1 = new HelloWord();
+        ob1.helloWord();
+        System.out.println(ob1.toString());
     }
 }
